2cac093fa3f1318712264941c6d920b665df0a7f
Thorsten Winkler <t.winkler@bigpoint.net>
2014-08-28 11:24:28 +0200

    This version is not compatible with mysql cookbook 5.4!

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,13 @@ maintainer_email 't.winkler@bigpoint.net'
 license          'All rights reserved'
 description      'A relational database server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.16'
+version          '0.0.17'
 
 depends          'apt', '>= 2.3.10'
 depends          'yum', '>= 3.2.2'
 depends          'mysql-chef_gem', '>= 0.0.2'
-depends          'database', '>= 2.1.8'
-depends          'mysql', '>= 5.0.0'
+depends          'database', '>= 2.3'
+depends          'mysql', '< 5.4'
 
 supports         'debian', '>= 7.0'
 supports         'centos', '>= 6.5'
